adedc61f1061f94a87b36c996ca4d114c1effd8e
Robert <robert.alexandrea7@gmail.com>
2022-06-27 19:27:31 -0300

    Criado modelo de negociação e instanciando no JS principal

--- ts_parte_1/typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++ ts_parte_1/typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -1,1 +1,5 @@
-alert('Oi');
+import { Negociacao } from './models/negociacao.js';
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao);
+console.log(negociacao.volume);
