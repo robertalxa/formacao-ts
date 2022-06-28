eb988980f06a001ebf1494a07f890787096885f4
Robert <robert.alexandrea7@gmail.com>
2022-06-27 21:52:35 -0300

    Tipadas as variaveis e integradas funcionalidades ao formulario

---
+++ ts_parte_1/typescript-curso-1-arquivos-iniciais/dist/js/controllers/negociacao.js
@@ -0,0 +1,22 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        return new Negociacao(this.inputData.valueAsDate, this.inputQuantidade.valueAsNumber, this.inputValor.valueAsNumber);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '1';
+        this.inputValor.value = '0.0';
+        this.inputData.focus();
+    }
+}
