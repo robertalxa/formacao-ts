104f660952ae22c54379d99eb42e25e641e38fab
Robert <robert.alexandrea7@gmail.com>
2022-07-12 18:18:03 -0300

    Aplicados conceitos de OO como classes e métodos abstratos e também trabalhando com tipo genérico dentro da classe de view

---
+++ ts-parte_2/typescript-curso-2/app/views/view.ts
@@ -0,0 +1,14 @@
+export abstract class View<T> { //Utilizando generics <T> para quando houver o import da classe view, seja a passado o tipo T para que possa ser utilizado nas outras funções
+    protected elemento: HTMLElement; //protected só deixa a classe e quem a herda utilizar o atributo
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    abstract template(model: T): string;
+
+    update(model: T): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
