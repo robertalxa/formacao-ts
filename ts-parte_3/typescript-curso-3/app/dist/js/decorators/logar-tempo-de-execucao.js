03c232948039c016cd98020b626031906f66e1f8
Robert <robert.alexandrea7@gmail.com>
2022-07-27 19:57:06 -0300

    Trabalhado o conceito de decorator

---
+++ ts-parte_3/typescript-curso-3/app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -0,0 +1,12 @@
+export function logarTempoDeExecucao() {
+    return function (target, propertyKey, descriptor) {
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function (...args) {
+            const t1 = performance.now();
+            const retorno = metodoOriginal.apply(this, args);
+            const t2 = performance.now();
+            console.log(`A função "${propertyKey}" executou em ${(t2 - t1) / 1000} segundos`);
+            retorno;
+        };
+    };
+}
