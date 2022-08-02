3278c2de5883d3d99510d223753ea6a85bca29ec
Robert <robert.alexandrea7@gmail.com>
2022-08-02 20:24:17 -0300

    Criado decorator para fazer um dom injector e utilizando cache para evitar processamento desnecessário

--- ts-parte_3/typescript-curso-3/app/dist/js/decorators/escape.js
+++ ts-parte_3/typescript-curso-3/app/dist/js/decorators/escape.js
@@ -3,7 +3,6 @@ export function escape() {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
             let retorno = metodoOriginal.apply(this, args);
-            console.log(`@escape em ação na class na classe ${this.constructor.name} para o método ${propertyKey}`);
             if (typeof retorno === 'string') {
                 retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
             }
--- ts-parte_3/typescript-curso-3/app/src/decorators/escape.ts
+++ ts-parte_3/typescript-curso-3/app/src/decorators/escape.ts
@@ -7,7 +7,7 @@ export function escape() {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args: any[]) {
             let retorno = metodoOriginal.apply(this, args);
-            console.log(`@escape em ação na class na classe ${this.constructor.name} para o método ${propertyKey}`);
+            // console.log(`@escape em ação na class na classe ${this.constructor.name} para o método ${propertyKey}`);
             
             if (typeof retorno === 'string') {
                 retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
